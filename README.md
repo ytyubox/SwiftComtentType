cb3082a86250b39bbccc80d93ef16bab566fb400
Tsungyu Yu <ytyubox@gmail.com>
2020-04-07 18:16:30 +0800

    update readme

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 ![Swift 5.1](https://img.shields.io/badge/Swift-5.1-orange.svg) 
 [![SPM](https://img.shields.io/badge/spm-compatible-brightgreen.svg?style=flat)](https://github.com/apple/swift-package-manager)
 
-A description of this package.
-
+```swift
+ContentType.json.value          // "application/json"
+ContentType.urlEncode.value     // "application/x-www-form-urlencoded"
+ContentType.formData.value      // "multipart/formdata"
+ContentType.plainText.value     // "text/plain"
+```
 ## Author
 
-AUTHOR_NAME, AUTHOR_EMAIL
+twitter : [@YuTsungYu](https://twitter.com/YuTsungYu) 
 
 ## License
 
--- Tests/SwiftContentTypeTests/SwiftContentTypeTests.swift
+++ Tests/SwiftContentTypeTests/SwiftContentTypeTests.swift
@@ -2,17 +2,17 @@ import XCTest
 @testable import SwiftContentType
 
 final class SwiftContentTypeTests: XCTestCase {
-        func testContentType() {
-					XCTAssertEqual(ContentType.json.value, "application/json")
-					XCTAssertEqual(ContentType.urlEncode.value, "application/x-www-form-urlencoded")
-					XCTAssertEqual(ContentType.formData.value, "multipart/formdata")
-					XCTAssertEqual(ContentType.plainText.value, "text/plain")
-				}
-	func testAttritubeContentType() {
-			XCTAssertEqual(ContentType.json("utf-8").value, "application/json ;charset=UTF-8")
+	func testContentType() {
+		XCTAssertEqual(ContentType.json.value, "application/json")
+		XCTAssertEqual(ContentType.urlEncode.value, "application/x-www-form-urlencoded")
+		XCTAssertEqual(ContentType.formData.value, "multipart/formdata")
+		XCTAssertEqual(ContentType.plainText.value, "text/plain")
 	}
-    static var allTests = [
-        ("testContentType", testContentType),
-				("testAttritubeContentType",testAttritubeContentType)
-    ]
+	func testAttritubeContentType() {
+		XCTAssertEqual(ContentType.json("utf-8").value, "application/json ;charset=UTF-8")
+	}
+	static var allTests = [
+		("testContentType", testContentType),
+		("testAttritubeContentType",testAttritubeContentType)
+	]
 }
