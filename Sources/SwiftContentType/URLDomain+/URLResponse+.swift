5e5dfc5b76c089939fe27c94e8e2b0f4a027353b
Tsungyu Yu <ytyubox@gmail.com>
2020-05-01 17:10:48 +0800

    add coder testing

--- Sources/SwiftContentType/URLDomain+/URLResponse+.swift
+++ Sources/SwiftContentType/URLDomain+/URLResponse+.swift
@@ -9,9 +9,29 @@ import SwiftcontentTypeCore
 import Foundation
 public
 extension HTTPURLResponse {
-	var contentType: AnyContentType? {
-		let value = allHeaderFields[AnyContentType.headerFaild] as? String
+  
+  var contentType: AnyContentType? {
+    let value = mimeType
     let contentTypeComponents = ContentTypeComponents(by: value)
     return contentTypeComponents.contentType
-	}
+  }
+  enum HttpResponseError:Error {
+    case notHttpResponse
+  }
 }
+
+import Combine
+extension URLSession.DataTaskPublisher {
+  public func mapToHttpURLResponse() ->  Publishers.TryMap<URLSession.DataTaskPublisher, (Data, HTTPURLResponse)> {
+    self.tryMap({ (arg0) -> (Data, HTTPURLResponse) in
+      
+      let (d, r) = arg0
+      guard let res = r as? HTTPURLResponse else {
+        throw HTTPURLResponse.HttpResponseError.notHttpResponse
+      }
+      return (d, res)
+    })
+  }
+}
+
+
--- Tests/SwiftContentTypeCoderTests/JSONCoderTests.swift
+++ Tests/SwiftContentTypeCoderTests/JSONCoderTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import Combine
 import SwiftContentTypeCoder
 
 class JSONCoderTests: XCTestCase{
@@ -24,6 +25,41 @@ class JSONCoderTests: XCTestCase{
     XCTAssertNotNil(jsonUTF8)
     XCTAssertEqual(jsonUTF8, #"{"a":"A"}"#)
   }
-  
+  func testFakeURLResponseAndBody() {
+    struct Jsonplaceholder: Codable {
+      let userID, id: Int
+      let title: String
+      let completed: Bool
+      
+      enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case id, title, completed
+      }
+    }
+    
+    let await = XCTestExpectation()
+    let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
+    let json:Data = #"""
+    {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+    }
+    """#.data(using: .utf8)!
+    let urlResponse = HTTPURLResponse(url: url, mimeType: "application/json", expectedContentLength: 123, textEncodingName: "UTF-8")
+    let sink =
+//      URLSession.shared.dataTaskPublisher(for: url)
+      Just<(Data,HTTPURLResponse)>((json,urlResponse))
+        .tryMap({ (d,r) -> Jsonplaceholder in
+          let contentType = r.contentType!
+          return try contentType.decoder.decode(Jsonplaceholder.self, from: d)
+        })
+        .sink(receiveCompletion: {_ in}) { (m) in
+          dump(m)
+          await.fulfill()
+    }
+    wait(for: [await], timeout: 10)
+  }
 }
 
